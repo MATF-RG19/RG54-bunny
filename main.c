5d1f435239defca89971131eb8715660f917429d
Julija-Damjanovic <julija.damjanovic.matf@gmail.com>
2020-07-21 18:11:42 +0200

    kreiranje prozora

---
+++ main.c
@@ -0,0 +1,44 @@
+#include <GL/glut.h>
+
+
+//Deklaracije callback funkcija
+
+static void on_display(void);
+
+
+int main(int argc,char **argv){
+    
+    //Inicijalizuje se GLUT
+    
+    glutInit(&argc,argv);
+    glutInitDisplayMOde(GLUT_RGB | GLUT_DOUBLE);
+    
+    
+    //Kreira se prozor
+    glutInitWindowSize(600,700);
+    glutInitWindowPosition(100,100);
+    glutCreateWindow(argv[0]);
+    
+    //Registruju se callback funkcije 
+    glutDisplayFunc(on_display);
+    
+    //Obavlja se OpenGL inicijalizacija
+    
+    glClearColor(0.8,0.8,0.8,0);
+    
+    //Program ulazi u glavnu petlju
+    
+    glutMainLoop();
+    
+    return 0;
+}
+
+static void on_display(void)
+{
+    //Brise se prethodni sadrzaj 'prozora'
+    glClear(GL_COLOR_BUFFER_BIT);   
+    
+    //Nova slika se salje na ekran 
+    glutSwapBuffers();
+    
+}
