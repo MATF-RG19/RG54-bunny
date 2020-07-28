265bd6da89947c88bdb47be36070f2d2a164ee1f
Julija-Damjanovic <julija.damjanovic.matf@gmail.com>
2020-07-29 00:16:16 +0200

    sirina i visina prozora, podesavanje viewporta,projekcije i tacke pogleda

--- main.c
+++ main.c
@@ -1,10 +1,14 @@
 #include <GL/glut.h>
 
 
+//Dimenzije prozora
+static int window_w, window_h;
+
+
+
 //Deklaracije callback funkcija
-
 static void on_display(void);
-
+static void on_reshape(int w,int h);
 
 int main(int argc,char **argv){
     
@@ -21,6 +25,7 @@ int main(int argc,char **argv){
     
     //Registruju se callback funkcije 
     glutDisplayFunc(on_display);
+    glutReshapeFunc(on_reshape);
     
     //Obavlja se OpenGL inicijalizacija
     
@@ -33,11 +38,44 @@ int main(int argc,char **argv){
     return 0;
 }
 
+
+
+
+
+
+static void on_reshape(int w,int h)
+{
+    //pamti se sirina i visina prozora
+    window_w = w;
+    window_h = h;
+
+}
+
+
+
 static void on_display(void)
 {
     //Brise se prethodni sadrzaj 'prozora'
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);   
     
+    //Podesavnje viewporta
+    glViewport(0,0,window_w,window_h);
+
+    /* Podesavanje projekcije*/
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    //prvi argument naredne fje-odredjuje ugao vidnog polja u stepenima u smeru y
+    //drugi-odnos koji odredjuje vidno polje,treci i cetvrti odredjuju rastojanje izmedju view i clipping plane 
+    gluPerspective(120,window_w/(float)window_h,1,6);
+
+    //Podesavanje vidne tacke
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    //1,2,3 -pozicija tacke od koje gledamo 0,0,0-polozaj referentne tacke i 0,1,0-pravac vektora
+    gluLookAt(1,2,3,0,0,0,0,1,0);
+
+
+
     //Nova slika se salje na ekran 
     glutSwapBuffers();
     
